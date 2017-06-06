0bb3f5e2f53d7ca6b48e5860ce3a04d0174fbb83
Christopher Mårtensson <cribalik@gmail.com>
2017-06-06 12:15:57 +0200

    Script to download dependencies

---
+++ get_libs.sh
@@ -0,0 +1,4 @@
+cd $(dirname $0)
+
+wget 'http://github.com/google/fonts/blob/master/apache/roboto/Roboto-Regular.ttf?raw=true' -O assets/Roboto-Regular.ttf
+wget 'https://openclipart.org/image/2400px/svg_to_png/248259/Charactervector.png' -O assets/spritesheet.png
